fd53a30deb7007ee38f8ea8acf0e0c531d20463d
Dany <dany@Danys-MacBook-Pro.local>
2021-02-19 16:56:42 -0600

    updates

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ const modalBtn = document.querySelectorAll(".modalBtn");
 const closeBtn = document.querySelectorAll(".closeBtn");
 
 // OPEN MODAL
-modal.forEach(function (element) {
-  modalBtn[0].onclick = function (event) {
-    modal[0].style.display = "block";
+for (let i = 0; i < modal.length; i++) {
+  modalBtn[i].onclick = function () {
+    modal[i].style.display = "block";
   }
-});
-
-modalBtn[1].onclick = function (event) {
-  modal[1].style.display = "block";
 }
 
 // CLOSE MODAL ON CLOSE BTN
-closeBtn[0].onclick = function () {
-  modal[0].style.display = "none";
-}
-
-closeBtn[1].onclick = function () {
-  modal[1].style.display = "none";
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].onclick = function () {
+    modal[i].style.display = "none";
+  }
 }
 
 // CLOSE MODAL ON WINDOW
